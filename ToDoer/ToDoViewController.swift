1ec1d889ec0231e169b961ad9df32f8345ab7586
Donald Nguyen <donxyen@gmail.com>
2019-02-16 01:01:28 -0600

    add new items functions

--- ToDoer/ToDoViewController.swift
+++ ToDoer/ToDoViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoViewController: UITableViewController {
 
-    let itemArray = ["1. find mike" , "2. buy eggos" , "3. destroy demogorgon"]
+    var itemArray = ["1. find mike" , "2. buy eggos" , "3. destroy demogorgon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -39,6 +39,26 @@ class ToDoViewController: UITableViewController {
         }
 
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    //MARK - Add New Items
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New ToDoEr Item", message: "Do it.", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (text) in
+            text.placeholder = "Create new item"
+            textField = text
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
     
 }
